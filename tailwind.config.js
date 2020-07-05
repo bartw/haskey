3587441d7c2c3b33503774e699f9b09b75c55224
Bart Wijnants <bart.w@sparkcentral.com>
2020-07-05 13:27:11 +0200

    season 20-21 refresh

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ["./public/**/*.html"],
+  },
+  theme: {
+    extend: {
+      colors: {
+        royal: {
+          100: "#f2f5fd",
+          200: "#a8baf0",
+          300: "#7d98e8",
+          400: "#5275e0",
+          500: "#2653d9",
+          600: "#1f42ad",
+          700: "#173282",
+          800: "#0f2157",
+          900: "#08112b",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
