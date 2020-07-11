48bcf50bbb54609d58cc739d000604068e61d6c6
Bart Wijnants <bart.w@sparkcentral.com>
2020-07-11 15:03:42 +0200

    tagline

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,10 @@ const Home = () => (
     <SEO title="Home" />
     <main className="flex flex-col h-screen items-center justify-center">
       <img alt="haskey-logo" className="w-64" src={haskeyLogo} />
-      <h1 className="uppercase text-4xl font-semibold text-center">
+      <h1 className="uppercase text-4xl font-semibold">
         Haskey Hasselt
       </h1>
+      <h2 className="uppercase">ijshockey voor jong en oud</h2>
     </main>
   </Layout>
 );
